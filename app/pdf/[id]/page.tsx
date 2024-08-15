f0234e251c0fcc711479903b4594b12b00ddd61b
Rahuletto <rahulmarban@gmail.com>
2024-08-16 01:10:39 +0530

    feat: get PDF in server side

---
+++ app/pdf/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { redirect } from "next/navigation";
+import React from "react";
+import { RiLoader3Fill } from "react-icons/ri";
+
+export default async function PDF({ params }: { params: { id: string } }) {
+  if (!params.id) return redirect("/");
+  else
+    await fetch(
+      `https://proscrape.vercel.app/api/dspace/getPDF?q=${params.id}`,
+      {
+        cache: "force-cache",
+        method: "POST",
+        body: JSON.stringify({
+          url: `http://dspace.srmist.edu.in/dspace/handle/123456789/${params.id}`,
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => data.redirect && redirect(data.redirect, "push" as any));
+
+  return (
+    <main className="flex h-screen w-screen animate-fadeIn flex-col items-center justify-center p-12 text-light-accent dark:text-dark-accent">
+      <RiLoader3Fill
+        title="loading"
+        className="animate-spin text-5xl font-medium text-light-accent dark:text-dark-accent"
+      />
+      <h1 className="mt-4 text-xl font-medium">Loading PDF...</h1>
+    </main>
+  );
+}

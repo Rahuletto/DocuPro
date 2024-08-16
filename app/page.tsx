b48dd472ec2269e8b9c6586fed686d0f4bef99c0
Rahuletto <rahulmarban@gmail.com>
2024-08-16 15:35:22 +0530

    chore: beta tag

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,24 @@ export default function Home() {
     <main className="transition-all duration-200 h-screen bg-light-background-normal dark:bg-dark-background-normal flex items-center justify-center flex-col gap-20">
       <ThemeToggle absolute />
       <div className="flex flex-col items-center justify-center gap-6">
-      <div className="transition-all duration-200 flex flex-col gap-1 items-center justify-center">
-        <div
-          id="title"
-          className="title transition duration-200 flex flex-col gap-1 dark:text-dark-color text-light-color"
-        >
-          <div className="flex gap-3 text-4xl items-end justify-center">
-            <IoLibrarySharp className="text-light-accent-color dark:text-dark-accent-color text-5xl" />
-            <h1 className="font-semibold">DocuPro</h1>
+        <div className="transition-all duration-200 flex flex-col gap-1 items-center justify-center">
+          <div
+            id="title"
+            className="title transition duration-200 flex flex-col gap-1 dark:text-dark-color text-light-color"
+          >
+            <div className="relative flex gap-3 text-4xl items-end justify-center">
+              <IoLibrarySharp className="text-light-accent-color dark:text-dark-accent-color text-5xl" />
+              <h1 className="font-semibold">DocuPro</h1>
+              <span className="text-light-accent-color dark:text-dark-accent-color rounded-md px-1 absolute top-0 -right-10 text-xs font-semibold bg-light-accent-background dark:bg-dark-accent-background">
+                BETA
+              </span>
+            </div>
           </div>
+          <p className="dark:text-dark-color text-light-color transition duration-200 opacity-60 text-lg font-medium">
+            Past exam papers at your fingertips.
+          </p>
         </div>
-        <p className="dark:text-dark-color text-light-color transition duration-200 opacity-60 text-lg font-medium">
-          Past exam papers at your fingertips.
-        </p>
-      </div>
-      <Search />
+        <Search />
       </div>
       <History />
 

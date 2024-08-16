d1a3e070e6ae6c99840958a80b3f7e7ae240ba60
Rahuletto <rahulmarban@gmail.com>
2024-08-16 15:20:11 +0530

    fix: newer conventions

--- app/pdf/[id]/file/route.tsx
+++ app/pdf/[id]/file/route.tsx
@@ -1,9 +1,7 @@
 // app/api/pdf/[id]/route.ts
 import { NextResponse } from "next/server";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge"
 
 export async function GET(
   request: Request,

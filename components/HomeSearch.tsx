b29376705eb4a745226629a3c70e62b4b307263e
Rahuletto <rahulmarban@gmail.com>
2024-09-06 12:38:14 +0530

    feat: saving the state of the mode

--- components/HomeSearch.tsx
+++ components/HomeSearch.tsx
@@ -1,21 +1,48 @@
-'use client'
-import React, {useState} from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 import Search from "@/components/SearchBar";
 
 export default function HomeSearch() {
-    const [papers, setPapers] = useState(false)
+  const [init, setInit] = useState(false);
+  const [papers, setPapers] = useState(false);
+  useEffect(() => {
+    const mode = localStorage.getItem("mode");
+    setInit(true);
+    if (mode) {
+      setPapers(mode === "resources");
+    }
+  }, []);
+  function onClick(bool: boolean) {
+    setPapers(bool);
+    localStorage.setItem("mode", bool ? "resources" : "qp");
+  }
   return (
-    <div className='flex flex-col gap-3'>
-        
-        <Search papers={papers} />
-        <div className='flex gap-2 w-full items-center justify-center'>
-            <button onClick={() => setPapers(false)} className={`${papers ? "dark:bg-dark-accent-background bg-light-accent-background text-light-accent-color dark:text-dark-accent-color" : "dark:bg-dark-accent-color bg-light-accent-color text-light-background-light dark:text-dark-background-darker"} transition duration-200 px-3 py-1 font-medium text-sm rounded-lg`}>
-                Search for semester papers
-            </button>
-            <button onClick={() => setPapers(true)} className={`${!papers ? "dark:bg-dark-accent-background bg-light-accent-background text-light-accent-color dark:text-dark-accent-color" : "dark:bg-dark-accent-color bg-light-accent-color text-light-background-light dark:text-dark-background-darker"} transition duration-200 px-3 py-1 font-medium text-sm rounded-lg`} >
-                Search for resources
-            </button>
+    <div className="flex flex-col gap-3">
+      <Search papers={papers} />
+      {init && (
+        <div className="flex gap-2 w-full items-center justify-center">
+          <button
+            onClick={() => onClick(false)}
+            className={`${
+              papers
+                ? "dark:bg-dark-accent-background bg-light-accent-background text-light-accent-color dark:text-dark-accent-color"
+                : "dark:bg-dark-accent-color bg-light-accent-color text-light-background-light dark:text-dark-background-darker"
+            } transition duration-200 px-3 py-1 font-medium text-sm rounded-lg`}
+          >
+            Search for semester papers
+          </button>
+          <button
+            onClick={() => onClick(true)}
+            className={`${
+              !papers
+                ? "dark:bg-dark-accent-background bg-light-accent-background text-light-accent-color dark:text-dark-accent-color"
+                : "dark:bg-dark-accent-color bg-light-accent-color text-light-background-light dark:text-dark-background-darker"
+            } transition duration-200 px-3 py-1 font-medium text-sm rounded-lg`}
+          >
+            Search for resources
+          </button>
         </div>
+      )}
     </div>
-  )
+  );
 }

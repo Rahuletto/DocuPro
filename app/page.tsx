c1466a3194155c774ac1a6c2fec9b87edb47b568
Rahul Marban <rahulmarban@gmail.com>
2025-01-31 18:10:47 +0530

    feat: sunsetting

--- app/page.tsx
+++ app/page.tsx
@@ -8,34 +8,8 @@ export default function Home() {
   return (
     <main className="transition-all duration-200 h-screen bg-light-background-normal dark:bg-dark-background-normal flex items-center justify-center flex-col gap-12">
       <ThemeToggle absolute />
-      <div className="flex flex-col items-center justify-center gap-6">
-        <div className="transition-all duration-200 flex flex-col gap-1 items-center justify-center">
-          <div
-            id="title"
-            className="title transition duration-200 flex flex-col gap-1 dark:text-dark-color text-light-color"
-          >
-            <div className="relative flex gap-3 text-4xl items-end justify-center">
-              <IoLibrarySharp className="text-light-accent-color dark:text-dark-accent-color text-5xl" />
-              <h1 className="font-semibold">DocuPro</h1>
-              <span className="text-light-accent-color dark:text-dark-accent-color rounded-md px-1 absolute top-0 -right-10 text-xs font-semibold bg-light-accent-background dark:bg-dark-accent-background">
-                BETA
-              </span>
-            </div>
-          </div>
-          <p className="dark:text-dark-color text-light-color transition duration-200 opacity-60 text-lg font-medium">
-            Past exam papers at your fingertips.
-          </p>
-        </div>
-        <HomeSearch />
-      </div>
-      <History />
-
-      <div className="transition duration-200 fixed bottom-4 mx-4 h-auto w-[98vw] animate-fadeIn flex-col gap-6 rounded-3xl bg-light-warn-background px-6 py-4 md:px-12 md:py-6 dark:bg-dark-warn-background">
-        <h3 className="text-light-warn-color dark:text-dark-warn-color">
-          We scrape the information from SRM DSpace, This website acts as a
-          wrapper and does not store any information whatsoever!
-        </h3>
-      </div>
+      <h1 className="font-semibold">Sunsetting</h1>
+      <p className="font-medium mx-3">We are sunsetting DocuPro in favour of ClassPro&apos;s Library functionality that syncs with your course list. with 900+ papers with 99.9% uptime.</p>
     </main>
   );
 }

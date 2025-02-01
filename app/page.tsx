35612a99eaa5515c1a5f70bd65c3cb42433f61a5
Rahul Marban <rahulmarban@gmail.com>
2025-02-01 09:00:03 +0530

    style: new styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="transition-all duration-200 h-screen bg-light-background-normal dark:bg-dark-background-normal flex items-center justify-center flex-col gap-12">
       <ThemeToggle absolute />
-      <h1 className="font-semibold">Sunsetting</h1>
-      <p className="font-medium mx-3">We are sunsetting DocuPro in favour of ClassPro&apos;s Library functionality that syncs with your course list. with 900+ papers with 99.9% uptime.</p>
+      <h1 className="font-semibold text-2xl">Sunsetting</h1>
+      <p className="font-medium mx-3 max-w-[400px]">We are sunsetting DocuPro in favour of ClassPro&apos;s Library functionality that syncs with your course list. with 900+ papers with 99.9% uptime.</p>
     </main>
   );
 }
